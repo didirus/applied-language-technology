779456f565303b6161ebae4e342b3ce94b18c7aa
diederusticus <diede.rusticus@nubisonline.nl>
2017-09-19 15:30:58 +0200

    Created dir

---
+++ Assignment1/src/main.py
@@ -0,0 +1,6 @@
+# Your task is to build a translation model. You have to implement and compute the following for
+# both data sets (clean and web):
+# 1. Phrase extraction algorithm
+# 2. Phrase translation probabilities
+# 3. Lexical translation probabilities (KMO approach)
+# 4. The resulting files of (1-3) can be combined into one single file of the form:
