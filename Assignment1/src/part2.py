5b24c96a8b34545a6962788d903afaec2b3a34c7
tushar1408 <tushar.1408@gmail.com>
2017-09-28 17:53:45 +0200

    Change of parameters

--- Assignment1/src/part2.py
+++ Assignment1/src/part2.py
@@ -1,28 +1,27 @@
 import numpy as np
 
-def phrase_probabilities(e=None, d=None, f_e=None,f_d=None, c_fe=None):
+def phrase_probabilities(freq_e=None,freq_f=None, c_freq=None):
 
     """
 
-    :param e: Phrase in english
-    :param d: Phrase in deu
-    :param f_e: Frequency of english phrase
-    :param f_d: Frequency of deu phrase
-    :param c_fe: Count when e-d allign
+    :param freq_e: Frequency of English phrase
+    :param freq_f: Frequency of deu phrase
+    :param c_freq: Count when e-d allign
     :return: Probability P(e|d) and P(d|e)
     """
 
-    p_ed = np.zeros(len(e),dtype=float)
-    p_de = np.zeros(len(e),dtype=float)
+
+    p_ef = np.zeros(len(freq_e),dtype=float)
+    p_fe = np.zeros(len(freq_f),dtype=float)
 
     for i in range(len(e)):
-        N_r = c_fe[i]
-        D_r1 = f_d[i]
-        D_r2 = f_e[i]
-        p_ed[i]= (N_r/D_r1)
-        p_de[i] = (N_r/D_r2)
+        N_r = c_freq[i]
+        D_r1 = freq_f[i]
+        D_r2 = freq_e[i]
+        p_ef[i]= (N_r/D_r1)
+        p_fe[i] = (N_r/D_r2)
 
-    return p_ed, p_de
+    return p_ef, p_fe
 # test
 e = np.array(['resumption', 'red','blue'])
 d = np.array(['rs','r','b'])
